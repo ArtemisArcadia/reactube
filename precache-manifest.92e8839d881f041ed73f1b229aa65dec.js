77552f787ed0be3857e5e38da333356b70009f48
OsmanAbdulMoomin <o.abdulmoomin@gmail.com>
2019-11-08 18:01:14 +0000

    Updates

---
+++ precache-manifest.92e8839d881f041ed73f1b229aa65dec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30dc8888779edd4c57020713ecd0a468",
+    "url": "/reactube/index.html"
+  },
+  {
+    "revision": "6fcdf2855ea379364c14",
+    "url": "/reactube/static/css/main.c43f38cc.chunk.css"
+  },
+  {
+    "revision": "c4f4178f2848dd01023f",
+    "url": "/reactube/static/js/2.5fdbe5e3.chunk.js"
+  },
+  {
+    "revision": "6fcdf2855ea379364c14",
+    "url": "/reactube/static/js/main.103fda2f.chunk.js"
+  },
+  {
+    "revision": "e159b1f80d1db463d372",
+    "url": "/reactube/static/js/runtime-main.8734d1ec.js"
+  }
+]);
